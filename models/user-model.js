c0feedd051060b4594bcaf0b32c5bd5ae4592b92
jrizza88 <jetorres88@gmail.com>
2019-05-01 00:44:27 -0400

    updated model for users

--- models/user-model.js
+++ models/user-model.js
@@ -34,7 +34,7 @@ function findByUsername(userName) {
 
 
 async function add(user) {
-    const [id] = await db('users').insert(user, ["id"])
+    const [id] = await db('users').insert(user, "id")
     return findById(id)
 }
 
