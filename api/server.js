f50d40ac463e3d4c705f5797a99cbb6e7f51e6fa
jrizza88 <jetorres88@gmail.com>
2019-05-01 20:37:10 -0400

    created search post by userid and created insert post request

--- api/server.js
+++ api/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 
-
 // all express middleware
 const middleware = require('./middleware');
 
 // add routes (from the auth folder) here (authorized route, user routes, etc.)
 const usersRouter = require('../routes/user-router');
+const postsRouter = require('../routes/posts-router');
 const authRouter = require('../routes/authentication-router');
 const server = express();
 
 middleware(server);
 
 
-server.use("/user", usersRouter);
+server.use("/api/user", usersRouter);
+server.use("/api/posts", postsRouter);
+// server.use("/api/comment", commentsRouter);
 server.use(authRouter)
 
 server.get('/', (req, res) => {
--- routes/posts-router.js
+++ routes/posts-router.js
@@ -2,6 +2,39 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const Posts = require('../models/post-model');
+const Users = require('../models/user-model');
+
 const { authenticate } = require('../auth/authenticate');
 
-const Posts = require('../models/post-model');
+
+// router.get('/all', (req, res) => {
+
+//     const { id } = req.params;
+//     Posts.findAllPostsByUser()
+//     .then(posts => {
+//         if(!posts){
+//             res.status(404).json({message: "this user has no posts..."})
+//         } else {
+
+//         }
+//     })
+// });
+
+router.post('/', (req, res) => {
+  const post = req.body;
+    Posts.addPost(post)
+    .then(newPost => {
+        res.status(201).json(newPost)
+    })
+    .catch( error => {
+        console.log("new post error", error)
+        res.status(500).json({ message: 'Could not post a new post'})
+    })
+})
+
+// router.get('/', (req, res) => {
+//     Posts.
+// })
+
+module.exports = router;
