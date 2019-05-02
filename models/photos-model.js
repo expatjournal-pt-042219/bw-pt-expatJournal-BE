c081ccd1885dd03334bed675ef1777df5c210790
jrizza88 <jetorres88@gmail.com>
2019-05-02 19:54:11 -0400

    completed photos model

---
+++ models/photos-model.js
@@ -0,0 +1,47 @@
+
+const db = require('../database/dbConfig')
+
+module.exports = {
+    findAllPhotosByUser,
+    findById,
+    findByPhoto,
+    findByUserId,
+    addPhoto,
+    deletePhoto,
+    updatePhoto
+}
+
+function findAllPhotosByUser(id){
+    return db('photos').where({'user_id': id});
+}
+
+function findById(id) {
+    return db('photos')
+    .where({ id })
+    .first();
+}
+
+function findByPhoto (filter) {
+    return db('photos').where(filter)
+}
+
+function findByUserId(id) {
+    return db('photos')
+    .leftJoin('users', 'users.user_id', 'photos.photos.id')
+    .where('photos.id', id)
+    .first();
+}
+
+async function addPhoto(photo) {
+       const [id] = await db('photos').insert(photo, 'id');
+       return findById(id)
+}
+
+function deletePhoto(id){
+    return db('photos').where('id', id).del()
+}
+
+async function updatePhoto(id, changes) {
+     await db('photos').where({ id }).update(changes)
+     return db('photos').where({ id }).first()
+}
