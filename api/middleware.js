0bbeafbc1bff68599fb7b3ddec8686342336bf3b
jrizza88 <jetorres88@gmail.com>
2019-05-02 19:08:15 -0400

    updated middleware file

--- api/middleware.js
+++ api/middleware.js
@@ -3,6 +3,8 @@ const helmet = require('helmet');
 const cors = require('cors');
 const logger = require('morgan');
 
+
+
 module.exports = server => {
     server.use(helmet());
     server.use(express.json());
