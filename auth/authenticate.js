e7b0c41952f3fb84010dc8a878aba9eb16f6bc2b
jrizza88 <jetorres88@gmail.com>
2019-05-02 22:58:16 -0400

    updated Authenticate files token'

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -6,14 +6,16 @@ const { jwtKey } = require('../secret/secret');
 
 const authenticate = (req, res, next) => {
     // const token = req.get('Authorization');
-    const token = req.headers.Authorization;
+    const token = req.headers.authorization;
+    // console.log(req)
+    console.log(req.headers.authorization)
     if (token) {
         jwt.verify(token, jwtKey, (err, decoded) => {
             if(err) 
             return res.status(401).json(err);
 
-            req.decoded = decoded;
-            // req.decodedJwt = decoded;
+            // req.decoded = decoded;
+            req.decodedJwt = decoded;
             next();
         });
     } else {
